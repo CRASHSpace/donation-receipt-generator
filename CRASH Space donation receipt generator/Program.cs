0e8a49e2ed23ce4f843a5a5fbfc4b3cbcab6fbe2
Max Pixel <git@m-pixel.com>
2020-04-05 15:10:07 -0700

    Blank C# console program

---
+++ CRASH Space donation receipt generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRASH_Space_donation_receipt_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
